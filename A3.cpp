7648cc15012873504772ff88d30c105e53578ae9
SentinelN1 <egor.a.antonov@gmail.com>
2023-03-11 20:09:26 +0300

    A3

--- A3.cpp
+++ A3.cpp
@@ -11,23 +11,25 @@ int main()
     is.open("input.txt");
     os.open("output.txt");
     is >> n;
-    std::vector<int> vec(n);
+    std::vector<long long int> vec(n);
     for (int i = 0; i < n; ++i)
     {
         is >> vec[i];
     }
+
     std::sort(vec.begin(), vec.end());
-    std::unique(vec.begin(), vec.end());
+    auto end = std::unique(vec.begin(), vec.end());
 
     is >> k;
     for (int i = 0; i < k; ++i)
     {
         int p;
         is >> p;
-        auto it = std::lower_bound(vec.begin(), vec.end(), p);
+        auto it = std::lower_bound(vec.begin(), end, p);
         int count = std::distance(vec.begin(), it);
         os << count << "\n";
     }
+
     is.close();
     os.close();
     return 0;
