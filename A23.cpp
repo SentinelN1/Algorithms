ad24d027df3c317455d00111aa37df2d8fe2020e
SentinelN1 <egor.a.antonov@gmail.com>
2023-03-13 04:54:05 +0300

    A23

--- A23.cpp
+++ A23.cpp
@@ -1,7 +1,16 @@
 // Количество треугольников
 #include <iostream>
+#include <vector>
 
 int main()
 {
-    
+    int n;
+    std::cin >> n;
+    std::vector<long long int> vec(n + 1);
+    vec[1] = 1;
+    for (long long int i = 2; i <= n; i++)
+    {
+        vec[i] = vec[i - 1] + i * (i + 1) / 2 + (i / 2) * (i - i / 2);
+    }
+    std::cout << vec[n];
 }
